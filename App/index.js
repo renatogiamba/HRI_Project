c2541c43409684697cefff447879ea42b16ef80b
diluccio.1797569 <diluccio.1797569@studenti.uniroma1.it>
2024-02-22 12:02:06 +0100

    Polishing index.js

--- App/index.js
+++ App/index.js
@@ -1,3 +1,31 @@
+const GREEN = "#6ef86e";
+const RED   = "#ff4a4a";
+
+let welcome = {
+    sceneName: "welcome",
+    text: () => 'Hello Human! Do you want to play a BlackJack match?',
+    buttons: ["Yes", "No"],
+    colors: [GREEN, RED],
+    listeners: [
+        () => {
+            ws.send(JSON.stringify({"buttonPressed": "Yes"}))
+            changeScene(presentation)
+        },
+        () => {
+            ws.send(JSON.stringify({"buttonPressed": "No"}))
+            changeScene(noGame)
+        }
+    ]
+}
+
+let waitingToStart = {
+    sceneName: "waitingToStart",
+    text: () => "Waiting for a human to play with me...",
+    buttons: [],
+    colors: [],
+    listeners: []
+}
+
 function textAnimation(sentence) {
     let target = document.querySelector('.welcome-text')
     let letterCount = 1
@@ -24,11 +52,16 @@ function changeScene(props, flag='') {
 
     buttonContainer.innerHTML = ''
 
-    if (currentScene != 'waitingToStart') {
-        ws.send(JSON.stringify({'vocabulary': props.buttons, 'sentence': props.text(), 'scene': props.sceneName}))
-    }
+    if (currentScene != "waitingToStart")
+        ws.send(JSON.stringify(
+            {
+                "vocabulary": props.buttons,
+                "sentence": props.text(),
+                "scene": props.sceneName
+            }
+        ))
     else {
-        ws.send(JSON.stringify({'message': 'interface started'}))
+        ws.send(JSON.stringify({"message": "interface started"}))
     }
 
     textAnimation(props.text())
@@ -117,50 +150,18 @@ function changeScene(props, flag='') {
     })
 }
 
-function main() {
-    ws = new WebSocket('ws://localhost:9050/websocketserver')
-    ws.onopen = function() {
-        console.log('Connection established')
-        ws.send('First Interaction')
-    }
-    ws.onmessage = function(event){
-        console.log(event.data)
-        humanMessage = JSON.parse(event.data)
 
-        if(humanMessage.answer == 'First Start'){
-            changeScene(waitingToStart)
-        }
-        else if (humanMessage.answer == 'start'){
-            changeScene(welcome)
-        }
-    }
-
-    let green = '#6ef86e'
-    let red = '#ff4a4a'
-
-    let welcome = {
-        sceneName: 'welcome',
-        text: () => 'Hello Human! Do you want to play a BlackJack match?',
-        buttons: ['Yes', 'No'],
-        colors: [green, red],
-        listeners: [() => {
-                        ws.send(JSON.stringify({'buttonPressed': 'Yes'}))
-                        changeScene(presentation)
-                    },
-                    () => {
-                        ws.send(JSON.stringify({'buttonPressed': 'No'}))
-                        changeScene(noGame)
-                    }
-                ]
-    }
-
-    waitingToStart = {
-       sceneName: 'waitingToStart',
-       text: () => 'Waiting for a human to play with me...',
-       buttons: [],
-       colors: [],
-       listeners: []
-    }
+ws = new WebSocket("ws://localhost:9050/websocketserver")
+ws.onopen = function() {
+    console.log("Connection established")
+    ws.send("First Interaction")
 }
+ws.onmessage = function(event) {
+    console.log(event.data)
+    humanMessage = JSON.parse(event.data)
 
-main()
+    if (humanMessage.answer == "First Start")
+        changeScene(waitingToStart)
+    else if (humanMessage.answer == "start")
+        changeScene(welcome)
+}
