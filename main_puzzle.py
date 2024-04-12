e211bc153bc680622b4267d5889845ba13ba3053
diluccio.1797569 <diluccio.1797569@studenti.uniroma1.it>
2024-04-12 11:17:23 +0200

    Done with Python server communication

--- main_puzzle.py
+++ main_puzzle.py
@@ -16,27 +16,19 @@ class NPuzzleWSServer(tornado.websocket.WebSocketHandler):
     def open(self):
         global n_puzzle_ws_server
         n_puzzle_ws_server = self
-        print('[Pepper N-Puzzle WS Server py]: Connection established')
+        print("[N-Puzzle WS Server py]: Connection established")
     
     @tornado.gen.coroutine
     def on_message(self, message):
-
         message = json.loads(message)
 
         if "gameTileMatrix" in message:
             problem = pddl_planning.generate_N_puzzle(message["gameTileMatrix"])
             moves = pddl_planning.solve_N_puzzle(problem)
-            #bankCardValue = convertValue(bankCardValue)
-            #playerCardValues = message["gameState"]["playerCardValues"]
-            #playerCardValues = list(map(convertValue,playerCardValues))
-            #obs = (
-            #    sum_hand(playerCardValues),
-            #    bankCardValue,
-            #    usable_ace(playerCardValues)
-            #) 
-            #action = agent.act(obs)
+            first_move = moves[1]
+            tile = int(first_move.split("(")[1].split(",")[0].split("_")[1])
             #pepper.say("I suggest: " + ("Stand" if action == 0 else "Hit"))
-            #self.write_message(json.dumps({"action": action}))
+            self.write_message(json.dumps({"move": tile}))
         #elif "pose" in message:
         #    if message["pose"] == "win":
         #        pepper.victory()
@@ -45,16 +37,38 @@ class NPuzzleWSServer(tornado.websocket.WebSocketHandler):
 
     @tornado.gen.coroutine
     def on_close(self):
-        print('[Pepper N-Puzzle WS Server py]: Connection closed')
+        print("[N-Puzzle WS Server py]: Connection closed")
   
     @tornado.gen.coroutine
     def on_ping(self, data):
-        print('ping received: %s' %(data))
+        print("ping received: %s" %(data))
   
     @tornado.gen.coroutine
     def on_pong(self, data):
-        print('pong received: %s' %(data))
+        print("pong received: %s" %(data))
   
     @tornado.gen.coroutine
     def check_origin(self, origin):
         return True
+
+if __name__ == "__main__":
+    npuzzle_web_app = tornado.web.Application([
+        (r"/websocketserver", NPuzzleWSServer)
+    ])
+    npuzzle_http_server = tornado.httpserver.HTTPServer(npuzzle_web_app)
+    npuzzle_http_server.listen("9020")
+    print("[N-Puzzle App py]: N-Puzzle WS server listening on port 9020")
+
+    try:
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        print("[N-Puzzle App py]: ---KeyboardInterrupt--- Quitting N-Puzzle WS server...")
+    
+    if n_puzzle_ws_server is not None:
+        try:
+            if n_puzzle_ws_server is not None:
+                n_puzzle_ws_server.write_message(json.dumps({"command": "close"}))
+                n_puzzle_ws_server.close()
+        except tornado.websocket.WebSocketClosedError:
+            pass
+    print("[N-Puzzle App py]: N-Puzzle WS server quit")
